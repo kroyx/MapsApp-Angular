f794a23794fb731fe4373b6aa83c85d85d79f3c1
Lelei <66555957+xLelei@users.noreply.github.com>
2023-05-27 12:26:16 +0200

    Diseño del menu de la aplicacion

--- src/app/maps/components/side-menu/side-menu.component.ts
+++ src/app/maps/components/side-menu/side-menu.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+export interface MenuItem {
+  name: string;
+  route: string;
+}
+
 @Component({
-  selector: 'app-side-menu',
+  selector: 'maps-side-menu',
   templateUrl: './side-menu.component.html',
   styleUrls: ['./side-menu.component.css']
 })
 export class SideMenuComponent {
 
+  public menuItems: MenuItem[] = [
+    { route: '/maps/fullscreen', name: 'FullScreen' },
+    { route: '/maps/zoom-range', name: 'Zoom-Range' },
+    { route: '/maps/markers', name: 'Markers' },
+    { route: '/maps/properties', name: 'Houses' },
+  ];
 }
