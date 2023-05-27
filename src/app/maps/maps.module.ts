4d0b50f7c7aade6c6071febcb81310eb63d4c6ad
Lelei <66555957+xLelei@users.noreply.github.com>
2023-05-27 18:26:31 +0200

    Diseño de la pantalla FullScreen

--- src/app/maps/maps.module.ts
+++ src/app/maps/maps.module.ts
@@ -1,14 +1,17 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
-import { MapsRoutingModule } from './maps-routing.module';
+import * as mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 import { MiniMapComponent } from './components/mini-map/mini-map.component';
 import { SideMenuComponent } from './components/side-menu/side-menu.component';
 import { MapsLayoutComponent } from './layout/maps-layout/maps-layout.component';
+import { MapsRoutingModule } from './maps-routing.module';
 import { FullScreenPageComponent } from './pages/full-screen-page/full-screen-page.component';
 import { MarkersPageComponent } from './pages/markers-page/markers-page.component';
 import { PropertiesPageComponent } from './pages/properties-page/properties-page.component';
 import { ZoomRangePageComponent } from './pages/zoom-range-page/zoom-range-page.component';
+
+(mapboxgl as any).accessToken = 'pk.eyJ1IjoieGxlbGVpIiwiYSI6ImNsaTV2OXlxOTB6a2szbW96bHBmZ3RzejgifQ.e7n02ZArPl935pLvSxa7ZA';
 
 
 @NgModule({
@@ -26,4 +29,4 @@ import { ZoomRangePageComponent } from './pages/zoom-range-page/zoom-range-page.
     MapsRoutingModule
   ]
 })
-export class MapsModule { }
+export class MapsModule {}
--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Map } from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 
 @Component({
   templateUrl: './full-screen-page.component.html',
-  styleUrls: ['./full-screen-page.component.css']
+  styleUrls: [ './full-screen-page.component.css' ]
 })
-export class FullScreenPageComponent {
+export class FullScreenPageComponent implements AfterViewInit {
+
+  @ViewChild('map') divMap?: ElementRef<HTMLElement>;
+
+  ngAfterViewInit(): void {
+
+    if(!this.divMap) throw 'El elemento HTML no fue encontrado';
+
+    const map = new Map({
+      container: this.divMap.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      // center: [ -74.5, 40 ], // starting position [lng, lat]
+      center: [ -3.7026354430291866, 40.40988823657996 ], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+  }
+
 
 }
